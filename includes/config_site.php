399fc1ea578022882e75332331bfb4aec3408420
Enic <enic23@gmail.com>
2015-03-18 23:12:07 +0100

    Revert Config site

--- includes/config_site.php
+++ includes/config_site.php
@@ -2,10 +2,10 @@
 
 // NewSystem DB connect
 //$newsdbhost = 'localhost';
-$newsdbhost = 'localhost';
-$newsdbuser = 'root';
-$newsdbpass = '';
-$newsdbname = 'devadmin_anime';
+$newsdbhost = '10.151.1.10';
+$newsdbuser = 'mainaftw_anime';
+$newsdbpass = '26V)YPh:|IJG';
+$newsdbname = 'mainaftw_anime';
 $x = mysql_connect($newsdbhost,$newsdbuser,$newsdbpass) or die(mysql_error());
 mysql_select_db($newsdbname,$x);
 ?>
