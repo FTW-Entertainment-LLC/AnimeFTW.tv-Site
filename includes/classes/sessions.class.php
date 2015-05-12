1c7d0765f25b917e226d6265f1023d74f4d909c7
Enic <brad@ftwentertainment.com>
2015-05-12 00:03:42 -0500

    Added Token Session handling class

---
+++ includes/classes/sessions.class.php
@@ -0,0 +1,161 @@
+<?php
+/****************************************************************\
+## FileName: sessions.v2.class.php									 
+## Author: Brad Riemann										 
+## Usage: Version 2.0 of the sessions class
+## Copywrite 2015 FTW Entertainment LLC, All Rights Reserved
+\****************************************************************/
+
+class Sessions extends Config {
+	
+	var $UserArray;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function setUserSessionData($id,$username,$rememberme)
+	{
+		// We need to set the following cookies to ensure that we are secure in what we do.
+		// cookies:
+		//	au - id of the user
+		//  hh - hash - shared secret, uid and mktime
+		//  vd - hash based on the user agent (just the browser) and the username
+		$randomkey = $this->generateRandomString(200);
+		$vdkey = MD5($this->getOS() . $username . "dcb93b6e8d4fdcc8be4bc95e61ee1a28" . time());
+		// Set the domain..
+		$ThisDomain = ".animeftw.tv";
+		if($_SERVER['HTTP_HOST'] == 'v4.aftw.ftwdevs.com')
+		{
+			$ThisDomain = ".ftwdevs.com";
+		}
+		// set the sessions
+		setcookie("au", $id, time() + (60*60*24*365), "/", $ThisDomain, 0, 1);
+		setcookie("hh", $randomkey, time() + (60*60*24*365), "/", $ThisDomain, 0, 1);
+		setcookie("vd", $vdkey, time() + (60*60*24*365), "/", $ThisDomain, 0, 1);
+		// set the information in the database.
+		$query = "INSERT INTO `" . $this->MainDB . "`.`user_session` (`id`, `added`, `updated`, `uid`, `agent`, `validate`) VALUES ('" . mysql_real_escape_string($vdkey) . "', '" . time() . "', '" . time() ."', '" . $id . "', '" . mysql_real_escape_string($_SERVER['HTTP_USER_AGENT']) . "', '" . mysql_real_escape_string($randomkey) . "')";
+		
+		$result = mysql_query($query);
+		if(!$result)
+		{
+			echo 'Error processing the update ' . mysql_error();
+			echo $query;
+		}
+	}
+	
+	public function checkUserSession()
+	{
+		// we need to check if the token and authentication are setup correctly.
+		$query = "SELECT COUNT(id) as `count` FROM `" . $this->MainDB . "`.`user_session` WHERE `id` = '" . mysql_real_escape_string($_COOKIE['vd']) . "' AND `uid` = '" . mysql_real_escape_string($_COOKIE['au']) . "' AND `validate` = '" . mysql_real_escape_string($_COOKIE['hh']) . "'";
+		$result = mysql_query($query);
+		$count = mysql_result($result, 0);
+		if($count > 0)
+		{
+			$query = "SELECT `Level_access`, `timeZone`, `Active`, `Username`, `canDownload`, `postBan`, `theme`, `forumBan`, `messageBan`, `viewNotifications`, `html5`, `ssl` FROM users WHERE ID='" . mysql_real_escape_string($_COOKIE['au']) . "'";
+			$result = mysql_query($query) or die('Error : ' . mysql_error());
+			$row = mysql_fetch_array($result);
+			$Logged = 1;
+			$globalnonid = mysql_real_escape_string($_COOKIE['au']);
+			$query = 'UPDATE users SET lastActivity=\''.time().'\' WHERE ID=\'' . mysql_real_escape_string($_COOKIE['au']) . '\'';
+			mysql_query($query) or die('Error : ' . mysql_error());
+			$PermissionLevelAdvanced = $row['Level_access'];
+			$timeZone = $row['timeZone'];
+			$bannedornot = $row['Active'];
+			$name = $row['Username'];
+			$canDownload = $row['canDownload'];
+			$postBan = $row['postBan'];
+			$siteTheme = $row['theme'];
+			$forumBan = $row['forumBan'];
+			$messageBan = $row['messageBan'];
+			$viewNotifications = $row['viewNotifications'];
+			$html5 = $row['html5'];
+			$ssl = $row['ssl'];
+		}
+		else
+		{
+			$Logged = 0;
+			$PermissionLevelAdvanced = 0;
+			$timeZone = '-6';
+			$canDownload = 0;
+			$siteTheme = 0;
+			$postBan = 0;
+			$name = '';
+			$bannedornot = 0;
+			$globalnonid = 0;
+			$forumBan = 0;
+			$messageBan = 0;
+			$viewNotifications = 0;
+			$html5 = 0;
+			$ssl = 0;
+		}
+		return array($Logged,$globalnonid,$PermissionLevelAdvanced,$timeZone,$bannedornot,$name,$canDownload,$postBan,$siteTheme,$forumBan,$messageBan,0,$viewNotifications,$html5,$ssl);
+	}
+	
+	private function getOS()
+	{
+		$os_platform    =   "Unknown OS Platform";
+		$os_array       =   array(
+			'/windows nt 10/i'     	=>  'Windows 10',
+			'/windows nt 6.3/i'     =>  'Windows 8.1',
+			'/windows nt 6.2/i'     =>  'Windows 8',
+			'/windows nt 6.1/i'     =>  'Windows 7',
+			'/windows nt 6.0/i'     =>  'Windows Vista',
+			'/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+			'/windows nt 5.1/i'     =>  'Windows XP',
+			'/windows xp/i'         =>  'Windows XP',
+			'/windows nt 5.0/i'     =>  'Windows 2000',
+			'/windows me/i'         =>  'Windows ME',
+			'/win98/i'              =>  'Windows 98',
+			'/win95/i'              =>  'Windows 95',
+			'/win16/i'              =>  'Windows 3.11',
+			'/macintosh|mac os x/i' =>  'Mac OS X',
+			'/mac_powerpc/i'        =>  'Mac OS 9',
+			'/linux/i'              =>  'Linux',
+			'/ubuntu/i'             =>  'Ubuntu',
+			'/iphone/i'             =>  'iPhone',
+			'/ipod/i'               =>  'iPod',
+			'/ipad/i'               =>  'iPad',
+			'/android/i'            =>  'Android',
+			'/blackberry/i'         =>  'BlackBerry',
+			'/webos/i'              =>  'Mobile'
+		);
+
+		foreach($os_array as $regex => $value)
+		{
+			if(preg_match($regex, $_SERVER['HTTP_USER_AGENT']))
+			{
+				$os_platform    =   $value;
+			}
+		}
+
+		return $os_platform;
+	}
+
+	private function getBrowser()
+	{
+		$browser        =   "Unknown Browser";
+		$browser_array  =   array(
+			'/msie/i'       =>  'Internet Explorer',
+			'/firefox/i'    =>  'Firefox',
+			'/safari/i'     =>  'Safari',
+			'/chrome/i'     =>  'Chrome',
+			'/opera/i'      =>  'Opera',
+			'/netscape/i'   =>  'Netscape',
+			'/maxthon/i'    =>  'Maxthon',
+			'/konqueror/i'  =>  'Konqueror',
+			'/mobile/i'     =>  'Handheld Browser'
+		);
+
+		foreach($browser_array as $regex => $value)
+		{
+			if(preg_match($regex,  $_SERVER['HTTP_USER_AGENT']))
+			{
+				$browser    =   $value;
+			}
+		}
+
+		return $browser;
+	}
+}
