af60f8559af347b68c693a7d56995c504e167083
Enic <brad@ftwentertainment.com>
2015-11-25 09:55:14 -0600

    Edit and Add watchlist entries for api v2.

--- includes/classes/watchlist.v2.class.php
+++ includes/classes/watchlist.v2.class.php
@@ -109,23 +109,141 @@ class watchlist extends Config {
 		$returneddata['total'] = $this->bool_totalWatchListEntries();
 		$returneddata['start'] = rtrim($start, ',');
 		$returneddata['count'] = $count;
-		$returneddata['sort'] = $this->Data['sort'];
+		if(isset($this->Data['sort'])) {
+			$returneddata['sort'] = $this->Data['sort'];
+		}
 		$i = 0;
-		
 		while($row = $result->fetch_assoc()) {
 			// a result was found, build the array for return.
 			foreach($row AS $key => &$value) {
-				$returneddata['results'][$i][$key] = $value;
-			}	
+				if(isset($this->Data['id']) && $row['tracker'] == 1 && $key == 'current_episode') {
+					// we ommit the current_episode option since the tracker is in play.
+				}
+				else {
+					if($key == 'currentep') {
+						$returneddata['results'][$i]['current_episode'] = $value;
+					}
+					else {
+						$returneddata['results'][$i][$key] = $value;
+					}
+				}
+			}
+			// If the request is for a specific My WatchList entry, we will pull down the proper layout for the various types.
+			if((isset($this->Data['id']) || isset($this->Data['sid'])) && $row['tracker'] == 1) {
+				// tracker was set, we send them
+				$returneddata['results'][$i]['tracker_information'] = $this->array_returnTrackerInformation($row['sid'],$row['tracker_latest']);
+			}
 			$i++;
 		}
 		return $returneddata;
 	}
 	
 	public function array_addWatchListEntry(){
+		if(isset($this->Data['id']) && is_numeric($this->Data['id'])){
+			// the ID was supplied, moving forward.
+			$query = "INSERT INTO `watchlist` (`id`, `uid`, `date`, `update`, `sid`, `status`, `email`, `currentep`, `tracker`, `tracker_latest`, `comment`) VALUES (NULL, '" . $this->UserID . "', '" . time() . "', '" . time() . "', '" . $this->mysqli->real_escape_string($this->Data['id']) . "', '0', '1', '0', '0', '0', '')";
+			$result = $this->mysqli->query($query);
+			if($result){
+				$results = array(
+					'id' => $this->mysqli->insert_id,
+					'date' => time(),
+					'updated' => time(),
+					'sid' => $this->Data['id'],
+					'status' => '1',
+					'email' => '1',
+					'current_episode' => '0',
+					'tracker' => '0',
+					'tracker_latest' => '0',
+					'comment' => '',
+				);
+				return array('status' => "200", 'message' => "Request successful.", 'results' => $results);
+			}
+			else {
+				return array('status' => "500", 'message' => "An issue appeared when attempting to run that request.");
+			}
+		}
+		else {
+			return array('status' => "501", 'message' => "There was a configuration issue with the request, ensure all requirements are met.");
+		}
 	}
 	
-	public function array_deleteWatchListEntry(){
+	public function array_editWatchListEntry(){
+		// we start off by checking to make sure the id of the WatchList entry was given, if it wasn't send them back.
+		if(isset($this->Data['id']) && is_numeric($this->Data['id'])){
+			// first we will query to get all of the existing details.
+			$query = "SELECT `status`, `email`, `currentep`, `tracker`, `tracker_latest`, `comment` FROM `watchlist` WHERE `id` = " . $this->mysqli->real_escape_string($this->Data['id']) . " AND `uid` = " . $this->UserID;
+			$result = $this->mysqli->query($query);
+			$existingRow = $result->fetch_assoc();
+			
+			// we will now go through the potential options that could be supplied to the system.
+			// check to see if the status changed. 
+			$querySetOptions = '';
+			$i = 0;
+			if(isset($this->Data['status'])){
+				if($this->Data['status'] != $existingRow['status']){
+					// the value doesn't match, so we need to update it.
+					$querySetOptions .= "`status` = '" . $this->mysqli->real_escape_string($this->Data['status']) . "', ";
+					$i++;
+				}
+			}
+			// check for email settings change.
+			if(isset($this->Data['email'])){
+				if($this->Data['email'] != $existingRow['email']){
+					$querySetOptions .= "`email` = '" . $this->mysqli->real_escape_string($this->Data['email']) . "', ";
+					$i++;
+				}
+			}			
+			// check for currentep updates
+			if(isset($this->Data['current_episode'])){
+				if($this->Data['current_episode'] != $existingRow['currentep']){
+					$querySetOptions .= "`currentep` = '" . $this->mysqli->real_escape_string($this->Data['current_episode']) . "', ";
+					$i++;
+				}
+			}			
+			// check for tracker options update
+			if(isset($this->Data['tracker'])){
+				if($this->Data['tracker'] != $existingRow['tracker']){
+					$querySetOptions .= "`tracker` = '" . $this->mysqli->real_escape_string($this->Data['tracker']) . "', ";
+					$i++;
+				}
+			}
+			// check for the tracker_latest option
+			if(isset($this->Data['tracker_latest'])){
+				if($this->Data['tracker_latest'] != $existingRow['tracker_latest']){
+					$querySetOptions .= "`tracker_latest` = '" . $this->mysqli->real_escape_string($this->Data['tracker']) . "', ";
+					$i++;
+				}
+			}
+			// Check if the comment was sent.
+			if(isset($this->Data['comment'])){
+				if($this->Data['comment'] != $existingRow['comment']){
+					$querySetOptions .= "`comment` = '" . $this->mysqli->real_escape_string($this->Data['comment']) . "', ";
+					$i++;
+				}
+			}
+			
+			// check to see if the returned values are the same as the stored ones.. it saves on the responses by the sql server..
+			if($i == 0){
+				// nothing to do.. no updates were made.
+				return array('status' => "200", 'message' => "Request successful. No Updates were made.");
+			}
+			else {
+				$querySetOptions = rtrim(rtrim($querySetOptions), ',');
+				// initiate the new query, there was an update.
+				$query = "UPDATE `watchlist` SET ${querySetOptions} WHERE `id` = " . $this->mysqli->real_escape_string($this->Data['id']) . " AND `uid` = " . $this->UserID;
+				$result = $this->mysqli->query($query);
+				
+				if(!$result){
+					return array('status' => "500", 'message' => "An issue appeared when attempting to run that request.");
+				}
+				else {
+					return array('status' => "200", 'message' => "Request successful.");
+				}
+			}
+		}
+		else {
+			return array('status' => "501", 'message' => "There was a configuration issue with the request, ensure all requirements are met.");
+		}
 	}
 	
 	private function bool_totalWatchListEntries(){
@@ -133,5 +251,21 @@ class watchlist extends Config {
 		$result = $this->mysqli->query($query);
 		$row = $result->fetch_assoc();
 		return $row['count'];
+	}
+	
+	private function array_returnTrackerInformation($sid,$tracker_latest) {
+		if($tracker_latest == '1') {
+			// latest episode watched.
+			$query = "SELECT `episode`.`epnumber` as `numrows` FROM `episode_tracker`, `episode` WHERE `episode_tracker`.`seriesName` = " . $sid . " AND `episode_tracker`.`uid` = " . $this->UserID . " AND `episode`.`id`=`episode_tracker`.`eid` ORDER BY `eid` DESC LIMIT 0, 1";
+		}
+		else {
+			// cumulative
+			$query = "SELECT COUNT(id) as `numrows` FROM `episode_tracker` WHERE `seriesName` = " . $sid . " AND `uid` = " . $this->UserID;
+		}
+		
+		$result = $this->mysqli->query($query);
+		$row = $result->fetch_assoc();
+		
+		return $row['numrows'];
 	}
 }
